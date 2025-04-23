3119c0270c508a4e370d1a359b07a44fb9dd81db
William A. <121400607+williamandrews2@users.noreply.github.com>
2025-04-23 13:59:31 -0500

    Created WeatherData class (saving remote progress).

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 const form = document.querySelector("form");
 const city = document.getElementById("searchbox");
 
+// testing variable
+let todaysWeather;
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   getWeather(city.value);
 });
+
+class WeatherData {
+  constructor(date, temperature, condition, icon) {
+    this.date = date;
+    this.temperature = temperature;
+    this.condition = condition;
+    this.icon = icon;
+  }
+}
 
 async function getWeather(city) {
   try {
@@ -12,8 +24,20 @@ async function getWeather(city) {
       `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=6TYEE487MLZ845HXHLDM9VE7C`
     );
     const weatherData = await response.json();
-    console.log(weatherData);
+    parseWeatherData(weatherData);
+    console.log(todaysWeather);
+    // console.log(weatherData);
   } catch {
     console.log(`Error retrieving weather data for the city of ${city}`);
   }
+}
+
+function parseWeatherData(data) {
+  // setting today's weather for testing
+  todaysWeather = new WeatherData(
+    data.days[0].datetime,
+    data.currentConditions.temp,
+    data.currentConditions.conditions,
+    data.currentConditions.icon
+  );
 }
